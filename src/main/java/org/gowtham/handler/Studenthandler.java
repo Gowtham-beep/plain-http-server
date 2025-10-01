a9a6375b8de66b76563f91ec63bbe0b9c4a8b302
GowthamN <gn86923@gmail.com>
2025-10-01 09:02:13 +0530

    Feat: handlePost request is done

--- src/main/java/org/gowtham/handler/Studenthandler.java
+++ src/main/java/org/gowtham/handler/Studenthandler.java
@@ -3,9 +3,13 @@ package org.gowtham.handler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.gowtham.entity.Student;
 import org.gowtham.repository.StudentRepository;
+import org.gowtham.request.CreateStudentRequest;
+import org.gowtham.response.StudentResponse;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URI;
 
 public class Studenthandler implements HttpHandler {
@@ -32,5 +36,27 @@ public class Studenthandler implements HttpHandler {
             exchange.sendResponseHeaders(405,-1);
         }
     }
+    private void handlePost(HttpExchange exchange)throws IOException{
+        CreateStudentRequest request = mapper.readValue(exchange.getRequestBody(),CreateStudentRequest.class);
+
+        Student student = new Student();
+        student.setName(request.getName());
+        student.setRollNo(request.getRollNo());
+
+        Student saved= repository.save(student);
+
+        StudentResponse response = new StudentResponse();
+        response.setId(saved.getId());
+        response.setName(saved.getName());
+        response.setRollNo(saved.getRollNo());
+
+        String jsonResponse = mapper.writeValueAsString(response);
+        exchange.getResponseHeaders().set("Content-Type","application/json");
+        exchange.sendResponseHeaders(201,jsonResponse.getBytes().length);
+        try(OutputStream os = exchange.getResponseBody()){
+            os.write(jsonResponse.getBytes());
+        }
+    }
+
 
 }
